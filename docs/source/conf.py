a097b1a79054e50fda55f692b8eafe052dd0ab9f
Claas <claas.rostock@dnvgl.com>
2023-02-21 22:08:09 +0100

    added test skeleton (though empty yet)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "furo"
-html_logo = "_static/mvf.svg"
+html_logo = "_static/mypackage.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,36 +9,28 @@ from pytest import LogCaptureFixture
 
 @pytest.fixture(scope="package", autouse=True)
 def chdir():
-    os.chdir(Path(__file__).parent.absolute() / "test_dicts")
+    os.chdir(Path(__file__).parent.absolute() / "test_output")
 
 
-mvf_dirs = [
-    "cases",
-    "dump",
-    "logs",
+output_dirs = [
     "results",
-    "templates",
 ]
-mvf_files = [
-    "sampled*",
-    "queueList*",
-    "*.copy",
-    "splash.png",
-    "caseList*",
+output_files = [
+    "*test*.pdf",
 ]
 
 
 @pytest.fixture(autouse=True)
 def default_setup_and_teardown(caplog: LogCaptureFixture):
-    _remove_mvf_dirs_and_files()
+    _remove_output_dirs_and_files()
     yield
-    _remove_mvf_dirs_and_files()
+    _remove_output_dirs_and_files()
 
 
-def _remove_mvf_dirs_and_files():
-    for folder in mvf_dirs:
+def _remove_output_dirs_and_files():
+    for folder in output_dirs:
         rmtree(folder, ignore_errors=True)
-    for pattern in mvf_files:
+    for pattern in output_files:
         for file in glob(pattern):
             file = Path(file)
             file.unlink(missing_ok=True)
